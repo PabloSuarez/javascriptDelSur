39292bd0dfaf80a90d9cb6cc6ba59fc79f271f0b
Pablo Suarez <hostsuarezpablo@gmail.com>
2015-12-21 21:35:17 -0300

    implement store.createRecord add in frontend

--- frontend/app/controllers/posts.js
+++ frontend/app/controllers/posts.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions: {
+    save: function () {
+      let _title = this.get('postTitle'),
+          _body  = this.get('postBody');
+
+      let post = this.store.createRecord('post', {
+        title: _title,
+        body: _body
+      });
+      post.save();
+
+    },
+  }
 });
